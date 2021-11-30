6cc6994549cff14f9ac5ab98d4384903b7776d7f
SreevidyaNattuva <75021824+SreevidyaNattuva@users.noreply.github.com>
2021-11-30 16:38:33 +0530

    Bananaprogram
    Bananaprogram

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
+//var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.jokes.one";
+function getTranslationURL(text)
+{
+  return serverURL+"?"+"text="+text
+}
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something went wrong with server, please try after sometime");
+}
+
 function clickHandler()
 {
-    console.log("clicked!");
-    console.log("input",txtInput.value);
+    //outputDiv.innerText = "Sree";
+    var inputText = txtInput.value;//Input from UI
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+         .then(response => response.json())
+         .then(json => {var translatedText = json.contents.translated;
+                        outputDiv.innerText=  translatedText})
+         .catch(errorHandler);
+
 };
 
 btnTranslate.addEventListener("click", clickHandler);
